3116b20dc9500b1aa27ad9985febb2f186570f1c
manojrai200 <manojrai9801@gmail.com>
2025-07-31 17:37:34 +0545

    pure react

--- part1/part1a/pure-react/src/App.js
+++ part1/part1a/pure-react/src/App.js
@@ -1,11 +1,28 @@
-const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Pixel Perfect Pizzas")
-  );
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
 };
 
-const react = document.getElementById("root");
-const root = ReactDOM.createRoot(react);
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Pixel Perfect Pizzas"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "The Big Meat Pizza",
+      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
